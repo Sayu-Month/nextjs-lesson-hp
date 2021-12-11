5eb11d92dd232afe912f5d202f3911bd1c71da39
Sayu-Month <60280696+Sayu-Month@users.noreply.github.com>
2021-12-12 07:41:37 +0900

    lessson2-10

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,6 +1,22 @@
-import Layout from '../components/Layout'
-const Blog = () => {
-  return <Layout title="Blog">Blog page</Layout>
-}
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
+
+const Blog = ({ posts }) => {
+  return (
+    <Layout title="Blog">
+      <ul>
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
+};
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
